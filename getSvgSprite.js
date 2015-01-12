7919c0076c77141a61de5981cec37edb1595d91c
Jon Yablonski <jon@jonyablonski.com>
2015-01-11 20:37:35 -0500

    getSvgSprite.js

---
+++ getSvgSprite.js
@@ -0,0 +1,8 @@
+var ajax = new XMLHttpRequest();
+ajax.open("GET", "svg/sprite.svg", true);
+ajax.send();
+ajax.onload = function(e) {
+  var div = document.createElement("div");
+  div.innerHTML = ajax.responseText;
+  document.body.insertBefore(div, document.body.childNodes[0]);
+}
