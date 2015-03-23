d7d20a2758ad64c1f90607460d2e942cd4c440d2
Jon Yablonski <jon@thinkmoncur.com>
2015-03-23 09:36:47 -0400

    Updated for Loop

--- forLoop.js
+++ forLoop.js
@@ -1,4 +1,14 @@
+/**
+ * for Loop
+ */
 for (var i = 0; i < elems.length; i++) {
-    console.log(i) // index
-    console.log(elems[i]) // object
+    console.log(i); // index
+    console.log(elems[i]); // object
+}
+
+// Exclude first item
+for (var i = 0; i < content.length; i++) {
+  if ( i !== 0) {
+    console.log(elems[i]); // object
+  }
 }
