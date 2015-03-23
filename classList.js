b1ff8a1cb79c180736ada3a79021b83533461d2e
Jon Yablonski <jon@thinkmoncur.com>
2015-03-23 09:25:23 -0400

    Added ClassList methods

---
+++ classList.js
@@ -0,0 +1,15 @@
+/**
+ * classList class methods
+ */
+
+// Adding a class
+element.classList.add("bar");
+
+// Removing a class
+element.classList.remove("foo");
+
+// Checking if has a class
+element.classList.contains("foo");
+
+// Toggle a class
+element.classList.toggle("active");
