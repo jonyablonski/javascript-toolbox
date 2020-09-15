a2d2baa369f16742383e738f86198e45a9e8d20c
Jon Yablonski <jon@jonyablonski.net>
2020-09-14 20:12:03 -0400

    added remove-array-duplicates

---
+++ remove-array-duplicates.js
@@ -0,0 +1,8 @@
+var sandwiches = ['turkey', 'ham', 'turkey', 'tuna', 'pb&j', 'ham', 'turkey', 'tuna'];
+
+var deduped = sandwiches.filter(function (sandwich, index) {
+	return sandwiches.indexOf(sandwich) === index;
+});
+
+// Logs ["turkey", "ham", "tuna", "pb&j"]
+console.log(deduped);
